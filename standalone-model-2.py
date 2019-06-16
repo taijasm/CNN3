bf6e40894f31c4ccdc6a375a761b011ee0ae68fb
taijasm <taijasmalpani@gmail.com>
2019-06-16 12:43:29 +0000

    Updating repo

--- standalone-model-2.py
+++ standalone-model-2.py
@@ -40,6 +40,10 @@ model.add(Conv2D(64, (3, 3)))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 
+model.add(Conv2D(64, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
 model.add(Flatten())
 model.add(Dense(64))
 model.add(Activation('relu'))
